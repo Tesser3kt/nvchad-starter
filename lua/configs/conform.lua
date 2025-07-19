f2d9118828cf43bd61085d0efb3c2e4336308a25
Adam Klepac <djklepy@gmail.com>
2025-07-19 15:25:42 +0200

    added black as formatter for python

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,12 +4,14 @@ local options = {
     css = { "prettierd" },
     html = { "prettierd" },
     js = { "prettierd" },
+    ts = { "prettierd" },
+    python = { "black" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_format = "fallback",
   },
 }
 
