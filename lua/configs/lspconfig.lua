98134d0709082afbce42870793dab7c985a5ad7d
Adam Klepac <djklepy@gmail.com>
2025-07-19 14:35:34 +0200

    mason hotfix

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -22,4 +22,24 @@ local servers = {
 }
 vim.lsp.enable(servers)
 
+-- Mason plugins
+require("lspconfig").html.setup{}
+require("lspconfig").cssls.setup{}
+require("lspconfig").bashls.setup{}
+require("lspconfig").ccls.setup{}
+require("lspconfig").docker_compose_language_service.setup{}
+require("lspconfig").dockerls.setup{}
+require("lspconfig").emmet_language_server.setup{}
+require("lspconfig").eslint.setup{}
+require("lspconfig").jsonls.setup{}
+require("lspconfig").lua_ls.setup{}
+require("lspconfig").marksman.setup{}
+require("lspconfig").nixd.setup{}
+require("lspconfig").pylsp.setup{}
+require("lspconfig").sqls.setup{}
+require("lspconfig").tailwindcss.setup{}
+require("lspconfig").texlab.setup{}
+require("lspconfig").ts_ls.setup{}
+require("lspconfig").tinymist.setup{}
+
 -- read :h vim.lsp.config for changing options of lsp servers
