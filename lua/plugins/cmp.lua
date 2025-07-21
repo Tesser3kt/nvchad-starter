44a42d35000c1fbbf764668a2f7a42a7b9f0836b
Adam Klepac <djklepy@gmail.com>
2025-07-21 22:47:37 +0200

    added higher priority to copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,12 +8,12 @@ return {
           priority = 2000,
         },
         {
-          name = "nvim_lsp",
-          priority = 1000,
+          name = "copilot",
+          priority = 1500,
         },
         {
-          name = "copilot",
-          priority = 750,
+          name = "nvim_lsp",
+          priority = 1000,
         },
         {
           name = "buffer",
