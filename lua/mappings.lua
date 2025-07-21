fd884b8b651fc8b32a9d88e6ab2616e0c519203d
Adam Klepac <djklepy@gmail.com>
2025-07-21 22:33:19 +0200

    added some custom mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal" })
+map("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { desc = "Correct last spelling mistake." })
 -- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
