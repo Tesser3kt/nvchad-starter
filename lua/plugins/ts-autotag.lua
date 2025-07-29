f957e7ea9858189a408fcdf1b1a8b73513bc3232
Adam Klepac <djklepy@gmail.com>
2025-07-29 14:55:13 +0200

    autotag hotfix

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -4,12 +4,16 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false, -- Auto close on trailing </
-  },
+  -- Defaults
+  opts = function()
+    require("nvim-ts-autotag").setup {
+      opts = {
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = true, -- Auto close on trailing </
+      },
+    }
+  end,
   -- Also override individual filetype configs, these take priority.
   -- Empty by default, useful if one of the "opts" global settings
   -- doesn't work well in a specific filetype
