81c2860e92c5f9d258ea56af7f18293994e34e95
Adam Klepac <djklepy@gmail.com>
2025-08-01 14:42:47 +0200

    debugging synctex

--- lua/options.lua
+++ lua/options.lua
@@ -13,7 +13,7 @@ g.lua_snippets_path = "~/.config/nvim/snippets/"
 g.vimtex_compiler_latexmk_engines = {
   ["_"] = "-pdf",
 }
-g.vimtex_view_method = "zathura"
+g.vimtex_view_method = "/home/tesserekt/zathura-debug.sh"
 g.vimtex_view_general_viewer = "/home/tesserekt/zathura-debug.sh"
 g.vimtex_compiler_progname = "nvr"
 g.vimtex_fold_enabled = 1
