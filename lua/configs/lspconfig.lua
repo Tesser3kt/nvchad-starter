43b88228787ce3c37689c88a54a1edd8c7337d97
Adam Klepac <djklepy@gmail.com>
2025-08-05 18:23:53 +0200

    setup arduino lsp for inkplate

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -24,3 +24,13 @@ local servers = {
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
+local MY_FQBN = "Inkplate_Boards:esp32:Inkplate10V2"
+vim.lsp.config("arduino-language-server", {
+  cmd = {
+    "arduino-language-server",
+    "-cli-config",
+    "$HOME/.config/nvim/lua/configs/arduino/arduino-cli.yaml",
+    "-fqbn",
+    MY_FQBN,
+  },
+})
