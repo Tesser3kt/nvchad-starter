456d100fa2825a8c0a894d4f4c8c9f4429fdb8ac
Adam Klepac <djklepy@gmail.com>
2025-10-26 22:33:27 +0100

    ccls nvchad setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -25,21 +25,25 @@ vim.lsp.enable(servers)
 -- read :h vim.lsp.config for changing options of lsp servers
 
 -- Ccls settings
-vim.lsp.config.ccls = {
-  cmd = { "ccls" },
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  root_dir = vim.lsp.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-  settings = {
-    ccls = {
-      cache = {
-        directory = ".ccls-cache",
-      },
-      highlight = {
-        lsRanges = true,
-      },
+local util = require "lspconfig.util"
+local server_config = {
+  filetypes = { "c", "cpp", "objc", "objcpp", "opencl" },
+  root_dir = function(fname)
+    return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
+      or util.find_git_ancestor(fname)
+  end,
+  init_options = {
+    cache = {
+      directory = vim.env.XDG_CACHE_HOME .. "/ccls/",
+      -- or vim.fs.normalize "~/.cache/ccls" -- if on nvim 0.8 or higher
     },
   },
+  --on_attach = require("my.attach").func,
+  --capabilities = my_caps_table_or_func
+}
+vim.lsp.config.ccls = {
   lsp = {
+    lspconfig = server_config,
     codelens = {
       enable = true,
       events = { "BufWritePost", "InsertLeave" },
