87ebca99c7faacee73cb8bfaec33c09867127b6d
Adam Klepac <djklepy@gmail.com>
2025-10-27 16:43:54 +0100

    switch to clangd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local servers = {
   "html",
   "cssls",
   "bashls",
-  "ccls",
+  "clangd",
   "docker_compose_language_service",
   "dockerls",
   "emmet_language_server",
@@ -23,30 +23,3 @@ local servers = {
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
-
--- Ccls settings
-local util = require "lspconfig.util"
-local server_config = {
-  filetypes = { "c", "cpp", "objc", "objcpp", "opencl" },
-  root_dir = function(fname)
-    return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
-      or util.find_git_ancestor(fname)
-  end,
-  init_options = {
-    cache = {
-      directory = vim.env.XDG_CACHE_HOME .. "/ccls/",
-      -- or vim.fs.normalize "~/.cache/ccls" -- if on nvim 0.8 or higher
-    },
-  },
-  --on_attach = require("my.attach").func,
-  --capabilities = my_caps_table_or_func
-}
-vim.lsp.config.ccls = {
-  lsp = {
-    lspconfig = server_config,
-    codelens = {
-      enable = true,
-      events = { "BufWritePost", "InsertLeave" },
-    },
-  },
-}
