257e0eb4c39a22e3b894876eee89d757803ee13b
Adam Klepáč <djklepy@gmail.com>
2026-02-14 15:17:54 +0100

    fixing claude

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      close_on_exit = false, -- so you can see errors too
+      direction = "float",
+    },
+  },
+}
+
