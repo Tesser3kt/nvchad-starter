02e606190153d21766ec2028f9e375d55902ff68
Adam Klepac <djklepy@pm.me>
2026-02-17 19:10:35 +0100

    add ufo

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    name = "ufo",
+    enabled = true,
+    dependencies = {
+      "kevinhwang91/promise-async", -- Required for git operations
+    },
+    config = function()
+      require("ufo").setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
+        end,
+      }
+    end,
+  },
+}
